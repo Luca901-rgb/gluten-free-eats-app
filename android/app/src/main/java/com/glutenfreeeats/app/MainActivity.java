409da5ac37f50ca72919142a50c64e378cae4a32
Luca901-rgb <lcammarota24@gmail.com>
2025-04-13 12:11:44 +0200

    Modificata pagina di login e registrazione, migliorato layout dell'app

---
+++ android/app/src/main/java/com/glutenfreeeats/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.glutenfreeeats.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.0406988df72c4fb89c66f80fcfc8a946',
+  appId: 'com.glutenfreeeats.app',
   appName: 'Gluten Free Eats',
   webDir: 'dist',
-  server: {
-    url: 'https://0406988d-f72c-4fb8-9c66-f80fcfc8a946.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+// server: {
+//   url: 'https://0406988d-f72c-4fb8-9c66-f80fcfc8a946.lovableproject.com?forceHideBadge=true',
+//   cleartext: true
+// },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
